9017c5684681080974fd61242fb1c39866430fb4
guka20 <gukadavitadze23@gmail.com>
2023-09-11 17:50:43 +0400

    Basic is done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
